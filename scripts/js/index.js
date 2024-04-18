c59a4e06a1fb016cdc2a7ccb113443fc8ea443a0
SledgeDew <sledgelozon@gmail.com>
2024-04-18 19:17:21 +0200

    first commit

---
+++ scripts/js/index.js
@@ -0,0 +1,187 @@
+import { recipes } from './dataBase/recipes.js'
+import { recipeTemplate } from './templates/recipeFactory.js'
+
+getRecipes()
+
+// sert à récupérer les recettes depuis recipes.js
+function getRecipes() {
+    const cardRecipe = document.querySelector('.hero')
+    cardRecipe.textContent = ''
+
+    const recipeModel = recipeTemplate()
+    for (const recipe of recipes) {
+        const recipeDOM = recipeModel.getRecipeDOM(recipe)
+        cardRecipe.appendChild(recipeDOM)
+    }
+    showCounterRecipes()
+}
+
+export function getFiltersIngredients() {
+    const cardFilter = document.getElementById('ingredients-choice')
+    cardFilter.innerHTML = ''
+
+    const filteredRecipes = applyFilters()
+    const getIngredients = () => {
+        const ingredients = []
+        filteredRecipes.forEach(recipe => {
+            recipe.ingredients.forEach(ingredient => {
+                if (!ingredients.includes(ingredient.ingredient.toLowerCase())) {
+                    ingredients.push(ingredient.ingredient.toLowerCase())
+                }
+            })
+        })
+        return ingredients
+    }
+
+    const allIngredients = getIngredients()
+
+    const filterModel = filterTemplate()
+    const filterDOM = filterModel.getFilterDOM(allIngredients, 'ingredient')
+    cardFilter.appendChild(filterDOM)
+}
+
+export function getFiltersAppliances() {
+    const cardFilter = document.getElementById('appliance-choice')
+    cardFilter.innerHTML = ''
+
+    const filteredRecipes = applyFilters()
+    const getAppliances = () => {
+        const appliances = []
+        filteredRecipes.forEach(recipe => {
+            if (!appliances.includes(recipe.appliance.toLowerCase())) {
+                appliances.push(recipe.appliance.toLowerCase())
+            }
+        })
+        return appliances
+    }
+    const allAppliances = getAppliances()
+    
+    const filterModel = filterTemplate()
+    const filterDOM = filterModel.getFilterDOM(allAppliances, 'appliance')
+    cardFilter.appendChild(filterDOM)
+        
+}
+
+export function getFiltersUstensils() {
+    const cardFilter = document.getElementById('ustensil-choice')
+    cardFilter.innerHTML = ''
+
+    const filteredRecipes = applyFilters()
+    const getUstensils = () => {
+        const ustensils = []
+        filteredRecipes.forEach(recipe => {
+            recipe.ustensils.forEach(ustensil => { // Accéder à chaque ustensil
+                if (!ustensils.includes(ustensil.toLowerCase())) {
+                    ustensils.push(ustensil.toLowerCase())
+                }
+            })
+        })
+
+        return ustensils
+        // à boucler dans le tableau des ustensiles
+    }
+    const allUstensils = getUstensils()
+
+    const filterModel = filterTemplate()
+    const filterDOM = filterModel.getFilterDOM(allUstensils, 'ustensil')
+    cardFilter.appendChild(filterDOM)
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ingredientSection = document.getElementById('filter-ingredient')
+    const applianceSection = document.getElementById('filter-appliance')
+    const ustensilSection = document.getElementById('filter-ustensil')
+    let ingredientOpened = false
+    let applianceOpened = false
+    let ustensilOpened =  false
+
+    ingredientSection.addEventListener('click', function(event) {
+        const divIngredient = document.getElementById('ingredients-choice')
+        const arrowElement = document.getElementById('ingredients-arrow')
+        // Vérifie si l'élément cliqué est l'icône "Ingrédients" ou son conteneur
+        if (event.target === ingredientSection
+            || event.target.classList.contains('sort__filter__categories__arrow')) {
+            if (ingredientOpened) {
+                arrowElement.classList.remove('sort__filter__categories__arrow__up')
+                divIngredient.classList.remove('open')
+                ingredientOpened = false
+            } else {
+                arrowElement.classList.add('sort__filter__categories__arrow__up')
+                divIngredient.classList.add('open')
+                getFiltersIngredients()
+                ingredientOpened = true
+            }            
+        }
+    })
+
+    applianceSection.addEventListener('click', function(event) {
+        const divAppliance = document.getElementById('appliance-choice')
+        const arrowElement = document.getElementById('appliance-arrow')
+        // Vérifie si l'élément cliqué est l'icône "Ingrédients" ou son conteneur
+        if (event.target === applianceSection
+            || event.target.classList.contains('sort__filter__categories__arrow')) {
+            if (applianceOpened) {
+                arrowElement.classList.remove('sort__filter__categories__arrow__up')
+                divAppliance.classList.remove('open')
+                applianceOpened = false
+            } else {
+                arrowElement.classList.add('sort__filter__categories__arrow__up')
+                divAppliance.classList.add('open')
+                getFiltersAppliances()
+                applianceOpened = true
+            }
+        }
+    })
+
+    ustensilSection.addEventListener('click', function(event) {
+        const divUstensil = document.getElementById('ustensil-choice')
+        const arrowElement = document.getElementById('ustensil-arrow')
+        // Vérifie si l'élément cliqué est l'icône "Ingrédients" ou son conteneur
+        if (event.target === ustensilSection
+            || event.target.classList.contains('sort__filter__categories__arrow')) {
+            if (ustensilOpened) {
+                arrowElement.classList.remove('sort__filter__categories__arrow__up')
+                divUstensil.classList.remove('open')
+                ustensilOpened = false
+            } else {
+                arrowElement.classList.add('sort__filter__categories__arrow__up')
+                divUstensil.classList.add('open')
+                getFiltersUstensils()
+                ustensilOpened = true
+            }
+        }
+    })
+})
+
+export function getTag(type, tag) {
+    const cardTag = document.querySelector('.tag')
+
+    const tagModel = addTagTemplate()
+    const tagDOM = tagModel.addTagDOM(type, tag)
+
+    tagDOM.addEventListener('click', () => {
+        selectFilter(type, tag)
+        cardTag.removeChild(tagDOM) // sert à supprimer le tag de l'interface
+        if (type === 'ingredient') {
+            getFiltersIngredients()
+        }
+        if (type === 'appliance') {
+            getFiltersAppliances()
+        }
+        if (type === 'ustensil') {
+            getFiltersUstensils()
+        }
+    })
+
+    cardTag.appendChild(tagDOM)
+}
+
+export function removeTag(tag) {
+    const tags = document.querySelectorAll('.tag__box')
+    tags.forEach(tagElement => {
+        if (tagElement.textContent === tag) {
+            tagElement.remove()
+        }
+    })
+}
---
+++ scripts/js/templates/recipeFactory.js
@@ -0,0 +1,75 @@
+export function recipeTemplate() {
+    function getRecipeDOM(recipe) {
+        const cardRecipe = document.querySelector('.hero')
+        const articleRecipeContainer = document.createElement('article')
+        const divRecipeImg = document.createElement('div')
+        const imgRecipe = document.createElement('img')
+        const sectionRecipe = document.createElement('section')
+        const pTimeRecipe = document.createElement('p')
+        const h2TitleRecipe = document.createElement('h2')
+        const divRecipeStep = document.createElement('div')
+        const h3Recipe = document.createElement('h3')
+        const pRecipeCook = document.createElement('p')
+        const sectionElementRecipe = document.createElement('section')
+        const h4ElementTitle = document.createElement('h4')
+        const ulElementRecipe = document.createElement('ul')
+
+        articleRecipeContainer.className = 'hero__container'
+        divRecipeImg.className = 'hero__container__img'
+        imgRecipe.className = 'hero__container__img__photo'
+        sectionRecipe.className = 'hero__container__recipe'
+        pTimeRecipe.className = 'hero__container__recipe__time'
+        h2TitleRecipe.className = 'hero__container__recipe__title'
+        divRecipeStep.className = 'hero__container__recipe__step'
+        h3Recipe.className = 'hero__container__recipe__step__title'
+        pRecipeCook.className = 'hero__container__recipe__step__cook'
+        sectionElementRecipe.className = 'hero__container__recipe__element'
+        h4ElementTitle.className = 'hero__container__recipe__element__title'
+        ulElementRecipe.className = 'hero__container__recipe__element__list'
+
+        // imgRecipe.setAttribute('alt', `${recipe.name}`)
+        imgRecipe.setAttribute('src', recipe.image)
+        pTimeRecipe.innerHTML = `${recipe.time} min`
+        h2TitleRecipe.innerHTML = `${recipe.name}`
+        h3Recipe.innerHTML = 'Recette'
+        pRecipeCook.innerHTML = `${recipe.description}`
+        h4ElementTitle.innerHTML = 'Ingrédients'
+
+        for (const ingredient of recipe.ingredients) {
+            const liElementRecipe = document.createElement('li')
+            liElementRecipe.className = 'hero__container__recipe__element__list__ingredient'
+            liElementRecipe.textContent = `${ingredient.ingredient}`
+            liElementRecipe.innerHTML += '<br>'
+
+            if (ingredient.quantity && ingredient.unit) {
+                const spanElementRecipe = document.createElement('span')
+                spanElementRecipe.className = 'hero__container__recipe__element__list__ingredient__quantity'
+                spanElementRecipe.textContent = `${ingredient.quantity} ${ingredient.unit}`
+                liElementRecipe.appendChild(spanElementRecipe)
+                liElementRecipe.innerHTML += ' '
+            } else if (ingredient.quantity) {
+                const spanElementRecipe = document.createElement('span')
+                spanElementRecipe.className = 'hero__container__recipe__element__list__ingredient__quantity'
+                spanElementRecipe.textContent = `${ingredient.quantity}`
+                liElementRecipe.appendChild(spanElementRecipe)
+            }
+            ulElementRecipe.appendChild(liElementRecipe)
+        }
+
+        cardRecipe.appendChild(articleRecipeContainer)
+        articleRecipeContainer.appendChild(divRecipeImg)
+        divRecipeImg.appendChild(imgRecipe)
+        articleRecipeContainer.appendChild(sectionRecipe)
+        sectionRecipe.appendChild(pTimeRecipe)
+        sectionRecipe.appendChild(h2TitleRecipe)
+        sectionRecipe.appendChild(divRecipeStep)
+        divRecipeStep.appendChild(h3Recipe)
+        divRecipeStep.appendChild(pRecipeCook)
+        sectionRecipe.appendChild(sectionElementRecipe)
+        sectionElementRecipe.appendChild(h4ElementTitle)
+        sectionElementRecipe.appendChild(ulElementRecipe)
+        return articleRecipeContainer
+    }
+
+    return {getRecipeDOM}
+}
